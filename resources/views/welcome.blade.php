37a8c17357b764b7d4c8e1ed6ed7ede2412fc816
IzzadSalleh <59683691+IzzadSalleh@users.noreply.github.com>
2020-11-04 21:29:42 +0800

    Update welcome.blade.php
    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,14 +85,14 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Shafiq</a>
+                    <a href="https://laracasts.com">Sayang</a>
+                    <a href="https://laravel-news.com">Siti</a>
+                    <a href="https://blog.laravel.com">And</a>
+                    <a href="https://nova.laravel.com">Iyah</a>
+                    <a href="https://forge.laravel.com">Forever</a>
+                    <a href="https://vapor.laravel.com">Till</a>
+                    <a href="https://github.com/laravel/laravel">Jannah</a>
                 </div>
             </div>
         </div>
